2095916d932a2d91c5fa4e863bde00dee65afddd
Dorian Bizgan <dorianbizgan@gmail.com>
2021-06-23 23:01:21 -0700

    Saving new practice solutions

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -7,37 +7,37 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
-            //    List<int> fibNums = new List<int>();
-            //    foreach (int n in new int[] { 5, 10, 20 })
-            //    {
+                List<int> fibNums = new List<int>();
+                foreach (int n in new int[] { 5, 10, 20 })
+                {
 
-            //        fibNums.Add(fib(n));
+                    fibNums.Add(fib(n));
 
-            //    }
+                }
 
-            //    foreach (int n in fibNums)
-            //    {
-            //        Console.WriteLine(n);
-            //    }
+                foreach (int n in fibNums)
+                {
+                    Console.WriteLine(n);
+                }
 
-            //}
+            
 
-            //static int fib(int n)
-            //{
-            //    if (n == 0)
-            //    {
-            //        return (0);
-            //    }
+            static int fib(int n)
+            {
+                if (n == 0)
+                {
+                    return (0);
+                }
 
-            //    if (n == 1)
-            //    {
-            //        return (1);
-            //    }
+                if (n == 1)
+                {
+                    return (1);
+                }
 
-            //    return (fib(n - 1) + fib(n - 2));
-            //}
+                return (fib(n - 1) + fib(n - 2));
+            }
 
-            //Reverse a string function
+            //# 1: Reverse a string function
 
             static string Reverser(string str)
             {
@@ -54,13 +54,14 @@ namespace CSharpPractice
                 return newString;
             }
 
-
+            //#1 Test
             Console.WriteLine("Reverser Test");
             Console.WriteLine(Reverser("Hello World"));
 
             Console.WriteLine();
             Console.WriteLine();
 
+            //#2 Test
             Console.WriteLine("Palindrome Test");
             Console.WriteLine(Palindrome("madam"));
             Console.WriteLine(Palindrome("charlie"));
@@ -69,9 +70,30 @@ namespace CSharpPractice
             Console.WriteLine();
             Console.WriteLine();
 
+            //#3 Test
+            Console.WriteLine(SentenceReverser("testing taht this is reversed"));
+            Console.WriteLine(SentenceReverser("testing another string to see if it's reversed and removes spaces"));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //#4 Test
+            Console.WriteLine(ReverseWordsIndividually("testing to see if each word is in same order but reversed individually"));
+            Console.WriteLine(ReverseWordsIndividually("testing something shorter"));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //#5 Test
+            CountCharacterOccurence("test");
+            CountCharacterOccurence("hello world");
+
+            Console.WriteLine();
+            Console.WriteLine();
 
         }
 
+        //#2
         private static string Palindrome(string v)
         {
             Console.WriteLine("---------------");
@@ -88,15 +110,73 @@ namespace CSharpPractice
             return (v == testString ? "Palindrome" : "Not Palindrome");
         }
 
-        //private static string SentenceReverser(string s)
-        //{
-        //    string[] words = s.Split(" ");
-        //    int numOfWords = words.Length - 1;
+        //# 3
+        private static string SentenceReverser(string s)
+        {
+            String reversed_String = "";
+            int oldStringLenght = s.Length;
 
-        //    foreach (numOfWords; numOfWords >= 0; numOfWords--) {
-        //        Console.WriteLine();
-        //    }
-        //    return "";
-        //}
+            for (int i = oldStringLenght - 1; i >= 0; i--)
+            {
+                char temp = s[i];
+                if (temp.ToString() == " ")
+                {
+                    continue;
+                }
+                else
+                {
+                    reversed_String += temp;
+                }
+            }
+            return reversed_String;
+        }
+
+        //# 4
+        private static string ReverseWordsIndividually(string s)
+        {
+            
+            string[] splitSentence = s.Split(" ");
+            List<string> newString = new List<string> {};
+
+            foreach (string temp in splitSentence)
+            {
+                int tempLength = temp.Length;
+                string tempString = "";
+                for (int i=tempLength - 1; i >= 0; i--) 
+                {
+                    tempString += temp[i];
+                }
+                newString.Add(tempString);
+            }
+
+            var result = string.Join(" ", newString.ToArray());
+            return result;
+        }
+
+        //#5 
+        private static void CountCharacterOccurence (string s)
+        {
+            Dictionary<char, int> charDict = new Dictionary<char, int> { };
+
+            foreach (char ch in s)
+            {
+                if (charDict.ContainsKey(ch))
+                {
+                    charDict[ch] += 1;
+                }
+                else
+                {
+                    charDict[ch] = 1;
+                }
+            }
+
+            foreach (var i in charDict)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+        }
+
+        //#6
     }
 }
